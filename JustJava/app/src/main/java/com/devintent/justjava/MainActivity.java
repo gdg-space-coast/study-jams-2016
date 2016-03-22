81c20009b5d3884fd7c930025b16db5d47a3aa65
Michael Prentice <splaktar@gmail.com>
2016-03-21 23:01:02 -0400

    update(main): use member variable for quantity

--- JustJava/app/src/main/java/com/devintent/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/devintent/justjava/MainActivity.java
@@ -11,6 +11,7 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +20,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
-        int quantity = 3;
+        quantity++;
         display(quantity);
     }
 
     public void decrement(View view) {
-        int quantity = 1;
+        quantity--;
         display(quantity);
     }
 
@@ -32,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 2;
         display(quantity);
         displayPrice(quantity * 5);
     }
